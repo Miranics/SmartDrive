c559a9bc01cd84ca5a26f45d99e51f3bec88a595
miranics <002nasya@gmail.com>
2025-11-25 07:52:16 +0200

    chore: add firebase-admin dependency to package.json

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:smartdrive/screens/login.dart';
 import 'package:smartdrive/services/auth_service.dart';
 import 'package:smartdrive/widgets/button_component.dart';
 import 'package:smartdrive/widgets/input.dart';
